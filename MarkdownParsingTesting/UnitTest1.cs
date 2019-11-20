d2befd5de069ce7a236234048029c3997879a2a8
Nathaniel Collier <drxs3v3n@gmail.com>
2019-11-19 19:49:52 -0600

    Experimenting with markdown traversal

--- MarkdownParsingTesting/UnitTest1.cs
+++ MarkdownParsingTesting/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MarkdownParsingTesting
@@ -8,9 +9,34 @@ namespace MarkdownParsingTesting
         [TestMethod]
         public void TestBasicImageFinding()
         {
-            var doc = Markdig.Markdown.Parse(System.IO.File.ReadAllText(@"../../examples/1.md"));
+            var testMarkdownText = System.IO.File.ReadAllText(@"../../../../markdownFiles/1.md");
+            var doc = Markdig.Markdown.Parse(testMarkdownText);
 
-            
+            foreach( var img in FindImages(doc))
+            {
+
+            }
+        }
+
+
+        private IEnumerable<Markdig.Syntax.MarkdownObject> FindImages(IEnumerable<Markdig.Syntax.MarkdownObject> objects)
+        {
+
+            foreach (var obj in objects)
+            {
+                if( obj is IEnumerable<Markdig.Syntax.MarkdownObject> subObjects)
+                {
+                    foreach( var subObj in FindImages(subObjects))
+                    {
+                        // is it an image
+                        yield return subObj;
+                    }
+                }else
+                {
+                    // is it an image
+                    yield return obj;
+                }
+            }
         }
     }
 }
